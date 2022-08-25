676f46aa9a3c86362eb30260bdc8b7d32d10c664
Josue Lourenco <josue_lourenco@hotmail.com>
2022-08-25 19:31:38 -0300

    Capturando Argumentos

--- src/test/java/br/ce/mjosuel/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/mjosuel/servicos/LocacaoServiceTest.java
@@ -13,10 +13,7 @@ import br.ce.mjosuel.utils.DataUtils;
 import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.internal.configuration.injection.MockInjection;
 import org.mockito.internal.exceptions.util.ScenarioPrinter;
 
@@ -294,5 +291,25 @@ public class LocacaoServiceTest {
 
         //acao
         service.alugarFilme(usuario,filmes);
+    }
+
+    @Test
+    public void deveProrrogarUmaLocacao(){
+
+        //cenario
+        Locacao locacao = LocacaoBuilder.umLocacao().agora();
+
+        //acao
+        service.prorrogarLocacao(locacao, 3);
+
+        //verificacao
+        ArgumentCaptor<Locacao> argCapt = ArgumentCaptor.forClass(Locacao.class);
+        Mockito.verify(dao).salvar(argCapt.capture());
+        Locacao locacaoRetornada = argCapt.getValue();
+
+        error.checkThat(locacaoRetornada.getValor(),is(12.0));
+        error.checkThat(locacaoRetornada.getDataLocacao(),ehHoje());
+        error.checkThat(locacaoRetornada.getDataRetorno(),ehHojeComDiferencaDias(3));
+
     }
 }
